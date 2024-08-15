67ee50f0f52012dc7c9f50a6d8ae7dfa313b79c0
aaljouilhy <aaljouilhy@gmail.com>
2024-08-15 20:55:38 +0300

    first commit

---
+++ studentidcard/student_id_card/doctype/student_entrance_license/student_entrance_license.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Student ID Card and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class StudentEntranceLicense(Document):
+	pass
